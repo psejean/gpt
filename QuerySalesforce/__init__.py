219d0e9fc9af337e41e27f83e55cce433249f9ea
psejean <psejea@lacitec.on.ca>
2024-08-30 10:32:30 -0400

    test

--- QuerySalesforce/__init__.py
+++ QuerySalesforce/__init__.py
@@ -3,6 +3,7 @@ import json
 import azure.functions as func
 from simple_salesforce import Salesforce
 
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     
